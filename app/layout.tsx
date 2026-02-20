9ea55dc4d0662fd63cd6cf6c176c7c24a653050a
KDP09SIGMA <hetkrishiv@gmail.com>
2026-02-20 19:02:50 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Source_Code_Pro } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+const sourceCodePro = Source_Code_Pro({
+  variable: "--font-source-code-pro",
+  subsets: ["latin"],
+});
+export const metadata: Metadata = {
+  title: "code-logic",
+  description:
+    "It is an paper checking system designed to help students improve their coding skills.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable}  ${sourceCodePro.variable} antialiased`}
+      >
+        {children}
+      </body>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
